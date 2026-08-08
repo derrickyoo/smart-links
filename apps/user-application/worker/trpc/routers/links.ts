29d7a8bc5080a50116a1fe48f5e8a3b2d41d28c2
Derrick Yoo <derrickyoo@gmail.com>
2026-08-07 20:11:33 -0700

    fix(user-application): imports and trpc procedures

--- apps/user-application/worker/trpc/routers/links.ts
+++ apps/user-application/worker/trpc/routers/links.ts
@@ -1,4 +1,9 @@
-// imports
+import { t } from "@/worker/trpc/trpc-instance";
+import { z } from "zod";
+import {
+  createLinkSchema,
+  destinationsSchema,
+} from "@repo/data-ops/zod-schema/links";
 import {
   createLink,
   getLink,
@@ -7,24 +12,28 @@ import {
   updateLinkName,
 } from "@repo/data-ops/queries/links";
 
-// update procedures
+import { TRPCError } from "@trpc/server";
+import { ACTIVE_LINKS_LAST_HOUR, LAST_30_DAYS_BY_COUNTRY } from "./dummy-data";
+
+export const linksTrpcRoutes = t.router({
   linkList: t.procedure
     .input(
       z.object({
         offset: z.number().optional(),
       }),
     )
-    .query(async ({ctx, input}) => {
-
-      return await getLinks(ctx.userInfo.userId, input.offset?.toString())
+    .query(async ({ ctx, input }) => {
+      return await getLinks(ctx.userInfo.userId, input.offset?.toString());
     }),
-  createLink: t.procedure.input(createLinkSchema).mutation(async ({ctx, input}) => {
-    const linkId = await createLink({
-      accountId: ctx.userInfo.userId,
-      ...input,
-    });
-    return linkId;
-  }),
+  createLink: t.procedure
+    .input(createLinkSchema)
+    .mutation(async ({ ctx, input }) => {
+      const linkId = await createLink({
+        accountId: ctx.userInfo.userId,
+        ...input,
+      });
+      return linkId;
+    }),
   updateLinkName: t.procedure
     .input(
       z.object({
@@ -33,8 +42,7 @@ import {
       }),
     )
     .mutation(async ({ input }) => {
-      console.log(input.linkId, input.name);
-      await updateLinkName(input.linkId, input.name)
+      await updateLinkName(input.linkId, input.name);
     }),
   getLink: t.procedure
     .input(
@@ -42,8 +50,8 @@ import {
         linkId: z.string(),
       }),
     )
-    .query(async ({input}) => {
-      const data = await getLink(input.linkId)
+    .query(async ({ input }) => {
+      const data = await getLink(input.linkId);
 
       if (!data) throw new TRPCError({ code: "NOT_FOUND" });
       return data;
@@ -56,5 +64,25 @@ import {
       }),
     )
     .mutation(async ({ input }) => {
-      await updateLinkDestinations(input.linkId, input.destinations)
+      await updateLinkDestinations(input.linkId, input.destinations);
     }),
+  activeLinks: t.procedure.query(async () => {
+    return ACTIVE_LINKS_LAST_HOUR;
+  }),
+  totalLinkClickLastHour: t.procedure.query(async () => {
+    return 13;
+  }),
+  last24HourClicks: t.procedure.query(async () => {
+    return {
+      last24Hours: 56,
+      previous24Hours: 532,
+      percentChange: 12,
+    };
+  }),
+  last30DaysClicks: t.procedure.query(async () => {
+    return 78;
+  }),
+  clicksByCountry: t.procedure.query(async () => {
+    return LAST_30_DAYS_BY_COUNTRY;
+  }),
+});
