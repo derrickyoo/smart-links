0acff4f76f3cc585469ad3a76f33fdd6e9d033cf
Derrick Yoo <derrickyoo@gmail.com>
2026-08-12 16:02:17 -0700

    feat(data-service): add producer to queue

--- apps/data-service/src/index.ts
+++ apps/data-service/src/index.ts
@@ -11,4 +11,10 @@ export default class DataService extends WorkerEntrypoint<Env> {
 	fetch(request: Request) {
 		return app.fetch(request, this.env, this.ctx);
 	}
+
+	async queue(batch: MessageBatch): void | Promise<void> {
+		for (const message of batch.messages) {
+			console.log('Queue Event:', message.body);
+		}
+	}
 }
