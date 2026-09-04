5567d46b96f2e6530fdc876ca11b94bf5b5220f8
Derrick Yoo <derrickyoo@gmail.com>
2026-09-03 17:44:31 -0700

    Fix worker config for evaluation scheduler

--- apps/data-service/src/index.ts
+++ apps/data-service/src/index.ts
@@ -3,6 +3,7 @@ import { QueueMessageSchema } from '@repo/data-ops/zod-schema/queue';
 import { WorkerEntrypoint } from 'cloudflare:workers';
 import app from './hono/app';
 import { handleLinkClick } from './queue-handlers/link-clicks';
+export { EvaluationScheduler } from '@/durable-objects/evaluation-scheduler';
 export { DestinationEvaluationWorkflow } from './workflows/destination-evaluation-workflow';
 
 export default class DataService extends WorkerEntrypoint<Env> {
